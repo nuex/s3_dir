fed79eac2a453ff03ef0c19fce90d88ad9e8d804
nuex <nx@nu-ex.com>
2013-12-18 03:51:21 -0500

    create an uploader class

--- lib/s3_dir.rb
+++ lib/s3_dir.rb
@@ -1,5 +1,6 @@
 require_relative 's3_dir/version'
 require 'fog/aws/storage'
+require 'digest/md5'
 
 # S3Dir uploads files to S3
 #
@@ -26,45 +27,62 @@ require 'fog/aws/storage'
 #     of its contents not public.
 #
 module S3Dir
+
   # Upload files to S3 
   def self.upload dir, key, options={}
-    files_path = File.expand_path(dir)
+    uploader = Uploader.new(dir, key, options)
+    uploader.upload
+  end
 
-    # Merge defaults with passed-in options
-    settings = {credential: ENV['FOG_CREDENTIAL'],
-                private: false}.merge(options)
+  class Uploader
+    attr_reader :key
+    attr_reader :bucket
+    attr_reader :storage
+    attr_reader :is_public
+    attr_reader :files_path
 
-    # Configure Fog
-    Fog.credential = settings[:credential]
+    def initialize dir, key, options
+      @files_path = File.expand_path(dir)
 
-    # Get a region
-    region = Fog.credentials[:region] || 'us-west-2'
+      # Merge defaults with passed-in options
+      settings = {credential: ENV['FOG_CREDENTIAL'],
+                  private: false}.merge(options)
 
-    # If we don't specify this endpoint, Fog will complain about
-    # not using the correct endpoint if the bucket has dots in
-    # the name (i.e. website bucket)
-    endpoint = 'http://s3.amazonaws.com'
+      # Configure Fog
+      Fog.credential = settings[:credential]
 
-    # This may be a public bucket
-    is_public = !settings[:private]
+      # Get a region
+      region = Fog.credentials[:region] || 'us-west-2'
 
-    # Set up our storage object
-    # We have to specify path_style here because Fog will complain about
-    # our website bucket (if we're using a bucket with dots in the name)
-    # not being covered by the SSL certificate.
-    fog_options = Fog.credentials.merge({provider: 'aws', path_style: true,
-                                         region: region, endpoint: endpoint})
-    fog_options.delete(:key_name)
-    storage = Fog::Storage.new(fog_options)
-    bucket = storage.directories.get(key)
-    bucket ||= storage.directories.create(key: key, public: is_public)
+      # If we don't specify this endpoint, Fog will complain about
+      # not using the correct endpoint if the bucket has dots in
+      # the name (i.e. website bucket)
+      endpoint = 'http://s3.amazonaws.com'
 
-    Dir.chdir(files_path) do
-      Dir['**/*'].each do |entry|
-        if File.directory?(entry)
-          bucket.files.create(key: entry, public: is_public)
-        else
-          bucket.files.create(key: entry, public: is_public, body: File.open(entry))
+      # This may be a public bucket
+      @is_public = !settings[:private]
+
+      # Set up our storage object
+      # We have to specify path_style here because Fog will complain about
+      # our website bucket (if we're using a bucket with dots in the name)
+      # not being covered by the SSL certificate.
+      fog_options = Fog.credentials.merge({provider: 'aws', path_style: true,
+                                           region: region, endpoint: endpoint})
+      fog_options.delete(:key_name)
+      @storage = Fog::Storage.new(fog_options)
+      @bucket = storage.directories.get(key)
+      @bucket ||= storage.directories.create(key: key, public: is_public)
+      @key = key
+    end
+
+    def upload
+      Dir.chdir(files_path) do
+        Dir['**/*'].each do |entry|
+          if File.directory?(entry)
+            bucket.files.create(key: entry, public: is_public)
+          else
+            bucket.files.create(key: entry, public: is_public, body: File.open(entry))
+          end
         end
       end
     end
